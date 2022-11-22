70dbfe4c7a5f6aed54264e571fecca2c9fbe81e4
Ida Oline Håberg <idh@equinor.com>
2022-11-22 21:28:49 +0100

    iterator for del calc all cases wip

--- fatigue_calc_all_cases.py
+++ fatigue_calc_all_cases.py
@@ -4,47 +4,46 @@ import matplotlib.pyplot as plt
 from wetb.fatigue_tools.rainflowcounting import rainflowcount
 import numpy as np
 import struct
+from get_moment_time_series import get_moment_time_series
 
 rainflow_windap = rainflowcount.rainflow_windap
 rainflow_astm = rainflowcount.rainflow_astm
 
-path = 'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank'
-#stress_params_file = path +  'DA_P53_CD.xlsx'
-fatigue_config_file = path +  'Doc-0081164-HAL-X-13MW-DGB-A-OWF-Detailed DLC List-Fatigue Support Structure Load Assessment_Rev7.0.xlsx'
-results_folder_for_cluster = path +  'Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations'
-DLC_ID = '12'
-#DLC_ID = '64a'
-#DLC_ID = '64b'
-#DLC_ID = '24a' 
-#DLC_ID = '31'
-#DLC_ID = '41a' 
-#DLC_ID = '41b'
-
+path = r'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank'
+fatigue_config_file = path +  r'\Doc-0081164-HAL-X-13MW-DGB-A-OWF-Detailed DLC List-Fatigue Support Structure Load Assessment_Rev7.0.xlsx'
+results_folder_for_cluster = path +  r'\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations'
+DLC_ID = 'DLC12'
+DLC_ID = 'DLC64a'
+DLC_ID = 'DLC64b'
 cluster_ID = 'JLO'
 
-moments = get_moment_time_series(DLC_ID, cluster_ID, fatigue_config_file, results_folder_for_cluster)
+moments_all_cases, weights = get_moment_time_series(DLC_ID, cluster_ID, fatigue_config_file, results_folder_for_cluster)
 
-binary_file = r'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations\1_Fatigue\F1.2_PowerProd\WD120\08\JLO12-08-120-150-00p70-04p33-0-s16.$105'
-text_file = r'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations\1_Fatigue\F1.2_PowerProd\WD120\08\JLO12-08-120-150-00p70-04p33-0-s16.%105'
+moments_all_cases_sector_1 = moments_all_cases[:,0,:]
+list_of_moments = []
+for timeseries in moments_all_cases_sector_1:
+    list_of_moments.append(timeseries)
 
+moment_lists = np.array([[weight, moment] for (weight, moment) in zip(weights, list_of_moments)])
+moment_lists = list(zip(weights, list_of_moments))
 
-content_reshaped = read_bladed_file(binary_file,text_file)
+print(np.shape(list_of_moments))
+print(np.shape(weights))
+print(np.shape(moment_lists))
+print(type(moment_lists))
 
-#plt.plot(content_reshaped[0,:])
+#plt.plot(moments_all_cases[1, 1, :])
 #plt.show()
+#damage_equivalent_moment = [eq_load(moment, no_bins=100, m=5, neq=1e7, rainflow_func=rainflow_windap) for moment in moments]
 
-moment_x = content_reshaped[0,:]
-moment_y = content_reshaped[1,:]
+eq_loads, cycles, ampl_bin_mean, ampl_bin_edges = eq_load_and_cycles(moment_lists, no_bins=46, m=[5], neq=[ 10 ** 7], rainflow_func=rainflow_windap)
 
-thetas = list(range(0,359,15))
+print(np.shape(eq_loads))
+print('eq_loads', eq_loads)
+#damage_equivalent_moment = np.reshape(damage_equivalent_moment, [1, len(damage_equivalent_moment)])[0]
 
-moments = [np.sin(theta)*moment_x+np.cos(theta)*moment_y for theta in thetas]
+#print(damage_equivalent_moment)
 
-damage_equivalent_moment = [eq_load(moment, no_bins=100, m=5, neq=1e7, rainflow_func=rainflow_windap) for moment in moments]
-
-eq_load_and_cycles(signals, no_bins=46, m=[5], neq=[ 10 ** 7], rainflow_func=rainflow_windap)
-
-
-damage_equivalent_moment = np.reshape(damage_equivalent_moment, [1, len(damage_equivalent_moment)])[0]
-
-print(damage_equivalent_moment)
+#DLC 12 = eq_loads [[25359289.980154537]]
+#DLC64a = eq_loads [[27978500.797801655]]
+#DLC64b = eq_loads [[20842588.818950817]]
