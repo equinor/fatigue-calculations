f9aab15eb8931aa102edb2b999ada88c3e32c50d
Ida Oline Håberg <idh@equinor.com>
2022-12-06 10:40:45 +0100

    multiprocess dem calc + plot dem

--- fatigue_calc_all_cases.py
+++ fatigue_calc_all_cases.py
@@ -5,30 +5,22 @@ from wetb.fatigue_tools.rainflowcounting import rainflowcount
 import numpy as np
 import struct
 from get_moment_time_series import get_moment_time_series
-
-rainflow_windap = rainflowcount.rainflow_windap
-rainflow_astm = rainflowcount.rainflow_astm
-
-path = r'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank'
-fatigue_config_file = path +  r'\Doc-0081164-HAL-X-13MW-DGB-A-OWF-Detailed DLC List-Fatigue Support Structure Load Assessment_Rev7.0.xlsx'
-results_folder_for_cluster = path +  r'\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations'
-
-DLC_ID = 'DLC12'
-#DLC_ID = 'DLC64a'
-#DLC_ID = 'DLC64b'
-#DLC_ID = 'DLC24a' 
-#DLC_ID = 'DLC31' # events
-#DLC_ID = 'DLC41a' # events 
-#DLC_ID = 'DLC41b' # events
-DLC_IDs = ['DLC12', 'DLC64a', 'DLC64b', 'DLC24a', 'DLC31', 'DLC41a', 'DLC41b'  ]
-
-timeseries_lengths = [1201, 1201, 1201, 1201, 1501, 201, 201]
-sectors = list((range(0,359,15)))
-cluster_ID = 'JLO'
+from multiprocessing import Pool
+import os 
 
 
-eq_loads_all_sectors = []
-for index, val in enumerate(sectors):
+
+def calculate_eq_loads_case_i(sector, sectors, DLC_IDs, timeseries_lengths, cluster_ID):
+
+    rainflow_windap = rainflowcount.rainflow_windap
+    rainflow_astm = rainflowcount.rainflow_astm
+
+    path = r'C:\Users\IDH\OneDrive - Equinor\R&T Wind\RULe\SSE Doggerbank'
+    fatigue_config_file = path +  r'\Doc-0081164-HAL-X-13MW-DGB-A-OWF-Detailed DLC List-Fatigue Support Structure Load Assessment_Rev7.0.xlsx'
+    results_folder_for_cluster = path +  r'\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations'
+
+
+    print( f'Process {os.getpid()} starts sector {sector}')
 
     weights_all_dlcs = []
     list_of_moments = []
@@ -36,7 +28,7 @@ for index, val in enumerate(sectors):
     for DLC_ID, timeseries_length in zip(DLC_IDs, timeseries_lengths):
         moments_all_cases, weights = get_moment_time_series(DLC_ID, cluster_ID, fatigue_config_file, results_folder_for_cluster, timeseries_length)
         
-        moments_all_cases_sector_1 = moments_all_cases[:,index,:]
+        moments_all_cases_sector_1 = moments_all_cases[:, sectors.index(sector),:]
         
         for timeseries in moments_all_cases_sector_1:
             list_of_moments.append(timeseries)
@@ -60,8 +52,32 @@ for index, val in enumerate(sectors):
     print('eq_loads', eq_loads)
 
     with open('dem_all_dlcs.txt', 'a') as resfile:
-        resfile.write('Sector' + str((val)) + str(eq_loads) + '\n')
+        resfile.write('Sector' + str((sector)) + str(eq_loads) + '\n')
 
-    eq_loads_all_sectors = eq_loads_all_sectors + eq_loads
+    return eq_loads
 
-print(eq_loads_all_sectors)
+def calculate_eq_loads_multiprocessed():
+    
+    pool_args = [(sector, sectors, DLC_IDs, timeseries_lengths, cluster_ID) for  sector in sectors]
+
+    # This corresponds to a for-loop over all cases
+
+    with Pool() as p:
+
+        results = p.starmap(calculate_eq_loads_case_i, pool_args)
+
+    return np.array(results) 
+
+
+if __name__ == '__main__':
+
+    
+    DLC_IDs = ['DLC12', 'DLC64a', 'DLC64b', 'DLC24a', 'DLC31', 'DLC41a', 'DLC41b'  ]
+
+    timeseries_lengths = [1201, 1201, 1201, 1201, 1501, 201, 201]
+    sectors = list((range(0,359,15)))
+    cluster_ID = 'JLO'
+
+    calculate_eq_loads_multiprocessed()
+
+
--- plot_del.py
+++ plot_del.py
@@ -6,6 +6,9 @@ dem_results_file = 'dem_all_dlcs_rainflow_astm.txt'
 SSE_reported_sectors = [np.deg2rad(165)]#Assuming that the max value from the report is the for the maximum sector from EQN computations
 SSE_reported_dem = [88041*10**3]
 
+matlab_dem = np.array([9.3158, 9.2559, 9.1866, 9.1266, 9.0702, 8.9909, 8.9180, 8.8910, 8.9525, 9.0681, 9.2277, 9.3201, 9.3158, 9.2559, 9.1866, 9.1266, 9.0702, 8.9909, 8.9180, 8.8910, 8.9525, 9.0852, 9.2277, 9.3153])*10**7
+
+
 with open(dem_results_file , mode='r') as file: # b is important -> binary
     lines = file.read().split('\n')
 
@@ -35,31 +38,39 @@ print('Worst sector', sectors_ap[np.argmax(vals_ap)])
 #PLOT
 ##########################################################
 
+print(max(vals_ap))
+
 sectors = list(np.deg2rad(range(0,359,15)))
 
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.plot(sectors, vals_astm)
 ax.plot(sectors, vals_ap)
+ax.plot(sectors, matlab_dem)
 ax.plot(SSE_reported_sectors , SSE_reported_dem , marker = 'o', color = 'r')#Values from report)
 #ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
 ax.set_rlabel_position(45)  # Move radial labels away from plotted line
 
-for s, v in zip(sectors[::2], vals_astm[::2]):
-    ax.annotate("{:.2e}".format(v), xy=[s, v], fontsize=10, color ='C0') 
+#for s, v in zip(sectors[np.argmax(np.array(vals_astm))], max(vals_astm)):
+ax.annotate("Max val: {:.2e}".format(max(vals_astm)), xy=[sectors[np.argmax(np.array(vals_astm))], max(vals_astm)], fontsize=10, color ='C0') 
 
-for s, v in zip(sectors[::2], vals_ap[::2]):
-    ax.annotate("{:.2e}".format(v), xy=[s+0.15, v+(v*0.05)], fontsize=10, color ='C1') 
+#for s, v in zip(sectors[np.argmax(np.array(vals_ap))], max(vals_ap)):
+ax.annotate("Max val: {:.2e}".format( max(vals_ap)), xy=[sectors[np.argmax(np.array(vals_ap))]+0.15,  max(vals_ap)+( max(vals_ap)*0.05)], fontsize=10, color ='C1') 
 
-for s, v in zip(SSE_reported_sectors, SSE_reported_dem):
-    ax.annotate("{:.2e}".format(v), xy=[s+0.2, v+(v*0.05)], fontsize=10, color ='r') 
+#for s, v in zip(sectors[np.argmax(np.array(matlab_dem))], max(matlab_dem)):
+ax.annotate("Max val: {:.2e}".format(max(matlab_dem)), xy=[sectors[np.argmax(np.array(matlab_dem))]+0.15, max(matlab_dem)+(max(matlab_dem)*0.05)], fontsize=10, color ='C2') 
+
+#for s, v in zip(SSE_reported_sectors, SSE_reported_dem):
+ax.annotate("SSE val: {:.2e}".format(max(SSE_reported_dem)), xy=[SSE_reported_sectors[np.argmax(np.array(SSE_reported_dem))]+0.2, max(SSE_reported_dem)-(max(SSE_reported_dem)*0.15)], fontsize=10, color ='r') 
 
 ax.set_theta_zero_location("N")  # theta=0 at the top
 ax.set_theta_direction(-1)  # theta increasing clockwise
-ax.set_rmax(np.max(vals_astm + vals_ap + SSE_reported_dem )*1.1)
+ax.set_rmax(np.max(vals_astm + vals_ap + SSE_reported_dem + list(matlab_dem) )*1.1)
+#ax.set_rmax(15**7)
+
 
 ax.grid(True)
 
 ax.set_title("DEM tower bottom", va='bottom')
-ax.legend(['rainflow_astm', 'rainflow_windap', 'SSE reported value'], loc='upper center', bbox_to_anchor=(0.5, -0.05)) 
+ax.legend(['rainflow_astm', 'rainflow_windap', 'matlab', 'SSE reported value'], loc='upper center', ncol=4,  bbox_to_anchor=(0.5, -0.05)) 
 plt.show()
 
