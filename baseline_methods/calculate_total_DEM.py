8b6e2c0db9022b40135b91e278c2cd54beac347d
Simen Sem Øvereng <SIOV@equinor.com>
2023-02-06 11:02:55 +0100

    Added comments to total DEM script to warn of damage of this kind only being applicable to members

--- baseline_methods/calculate_total_DEM.py
+++ baseline_methods/calculate_total_DEM.py
@@ -87,18 +87,22 @@ if __name__ == '__main__':
                 tot_weighted_DEM_all_angles = np.zeros_like(weighted_DEM_sum_DLC_i)
             
             tot_weighted_DEM_all_angles += weighted_DEM_sum_DLC_i
-            
+        
+        logger.info(f'Added all the weighted DEM sums together for all DLCs')
+        # When DEM-sums from all DLCs  has been looped through    
         if method == 'python':
             df_out = pd.DataFrame(tot_weighted_DEM_all_angles, 
                                   index = [f'{geo_matrix[key]["elevation"]:1f}' for key in geo_matrix.keys()], 
                                   columns = [f'{a:.1f}' for a in point_angles[:]])
             df_out.to_excel(out_path_xlsx, index_label = 'mLat')
-            print(df_out)
-                
+            logger.info(f'Stored the python method results to {out_path_xlsx}')
+     
         for geo_idx, geo_row in enumerate(tot_weighted_DEM_all_angles):
             '''
-            TODO map the geo idx to the DataFrame indexes after having dropped some geometries
-            E.g, solution can be as below, but i suspect the "idx" must be rescaled in the geometry df
+            NOTE: the geo_idx is not mapped to the DataFrame indexes if dropping some geometries
+            NOTE 06.02.23 - this script is not being used for precise calculations, so it is not relevant to update
+            
+            E.g, solution can be as below, but suspect that the "idx" must be rescaled in the geometry df
             for geo_idx in range(geometry.shape[0]): 
                 geo_row = geometry.iloc[geo_idx]
                 
@@ -108,7 +112,6 @@ if __name__ == '__main__':
             
             actual_angles_geo_i = point_angles[:]
             compass_angles_geo_i = compass_angles[:]
-            # test_df = pd.DataFrame(data = [geo_row], columns = [f'DEM {i} deg' for i in point_angles])
             DEM_geo_i_all_angles = tot_weighted_DEM_all_angles[geo_idx]
             DEM_max = (T_lifetime / N_equivalent * DEM_geo_i_all_angles.max() )**(1 / wohler_exp)
             
@@ -118,18 +121,21 @@ if __name__ == '__main__':
             if method_idx == 0:
                 df_row_labels.append(f'Elevation {el:.2f} mLat')
             
-     
     # Add values from the c2wind final result verification, and print the 
     res_data['c2wind'] = [88.92, 98.21, 102.18, 104.06]
     vals = np.array(list(res_data.values())).T
-    pd.set_option("display.precision", 2)
     df = pd.DataFrame(vals, index = df_row_labels, columns = [f'DEM {m} [MNm]' for m in res_data.keys()])  
     logger.info(f'Comparison of largest DEM found at each elevation per method') 
-    print(df)
+    with pd.option_context('display.precision', 2):
+        print(df)
     sys.exit()
     
-    logger.info(f'Finding overall max DEM of worst geo/elevation for the python method')
     # TODO this is just because the python method is the last one in the methods list
+    
+    
+    logger.info(f'Finding overall max DEM of worst geo/elevation for the python method')
+    # TODO here information about the SCF and specific angles needs to be used -> all angles must be evaluated!
+    # The code below is not valid
 
     worst_elevation_idx = divmod(tot_weighted_DEM_all_angles.argmax(), tot_weighted_DEM_all_angles.shape[1])[0] # Finds the row idx of the row containing the element with the lowest value of entire array
     worst_overall_sector_idx = tot_weighted_DEM_all_angles[worst_elevation_idx].argmax()
@@ -145,14 +151,11 @@ if __name__ == '__main__':
     print(f'Elevation {mLAT_at_worst_elevation:.2f} mLAT')
     print(f'Max DEM = {worst_overall_DEM / 10**6:.2f} MNm at {actual_angles_worst_el[worst_overall_sector_idx]} deg / {compass_angles_worst_el[DEM_geo_i_all_angles.argmax()]:.0f} degN')
 
+    sys.exit()
+    # NOTE Code below is not relevant for this script since we do not have utilization data for the members we have time series data for  
+    
     # Calculate damage, and lifetime (hard to compare right now since the % utilization for existing geometries is not available)
     equivalent_stress = worst_overall_DEM * geo_matrix[worst_elevation_idx]['D'] / (2 * geo_matrix[worst_elevation_idx]['I'])
-    
-    # TODO here information about the SCF and specific angles needs to be used -> all angles must be evaluated!
-    # The code below is not valid
-    
-    
-    # test = out_df[geo_idx]['scf_per_point']
     
     equivalent_stress /= 10**6 # scale to MPa for use with SN-curves or others
     dmg = curve.miner_sum(np.array([[equivalent_stress, N_equivalent]])) * DFF
