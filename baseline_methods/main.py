cde2587912ea3c6d504f2a4aa26b04776f75348e
Simen Sem Øvereng <SIOV@equinor.com>
2023-02-09 10:52:18 +0100

    Update main.py with better readability -> changed to sector and cycles etc

--- baseline_methods/main.py
+++ baseline_methods/main.py
@@ -1,4 +1,3 @@
-from utils.SN_Curve import SN_Curve_qats
 from utils.extract_and_preprocess_data import extract_and_preprocess_data
 from utils.setup_custom_logger import setup_custom_logger
 from utils.IO_handler import store_table
@@ -37,7 +36,7 @@ if __name__ == '__main__':
     data_path              = fr'{os.getcwd()}\data'
     DLC_file               = data_path +  r'\Doc-0081164-HAL-X-13MW-DGB-A-OWF-Detailed DLC List-Fatigue Support Structure Load Assessment_Rev7.0.xlsx'
     sim_res_cluster_folder = data_path +  r'\Doc-0089427-HAL-X-13MW DB-A OWF-ILA3_JLO-model_fatigue_timeseries_all_elevations'
-    geometry_file          = data_path +  r'\DA_P53_CD.xlsx'
+    geometry_file_members  = data_path +  r'\DA_P53_CD_members.xlsx'
     
     ###
     ### -!-!-!-!- NOTE CONFIG VARIABLES START
@@ -49,46 +48,44 @@ if __name__ == '__main__':
         
     MULTIPROCESS = True # False if using only single processor, which should be done if debugging, plotting etc.
     DLC_IDs      = ['DLC12', 'DLC24a',  'DLC31', 'DLC41a', 'DLC41b', 'DLC64a', 'DLC64b']
-    geometry     = pd.read_excel(geometry_file)#.drop([0,1], axis=0) # TODO .drop() can be used for testing only a set of geometries
-    point_angles = [float(i) for i in range(0,359,15)] # evenly distributed angles in the turbine frame. All iterations are done with these, but each geometry might have sectors / angles that deviate some
-    store_ranges = True
+    geometry     = pd.read_excel(geometry_file_members)#.drop([0,1], axis=0) # TODO .drop() can be used for testing only a set of geometries
+    sectors      = [float(i) for i in range(0,359,15)] # evenly distributed angles in the turbine frame
+    store_cycles = True # store rainflow cycles
     ###
     ### -!-!-!-!- NOTE CONFIG VARIABLES END
     ###
     
-    # TODO transform between "global turbine frame" and the compass frame, as the SCF angles are given in the latter
-    n_geometries       = geometry.shape[0]
-    cluster_ID         = 'JLO'
-    out_file_type      = 'mat' # or npy 
-    logger             = setup_custom_logger('Main') # logger.info('Message') and logger.error('Message')
-    rainflow_func      = get_range_and_count_qats
-    curve              = SN_Curve_qats('D')
-    geo_matrix         = create_geo_matrix(geometry, point_angles, curve) # better matrix to pass to the main function
-           
-    info_str  = "DEM" if DEM else "damage"
-    calc_func = calculate_DEM_case_i if DEM else calculate_damage_case_i
-    factor    = 6.0 # to go from damage or DEM "per 10 min" to "per hr" 
+    n_geometries  = geometry.shape[0]
+    cluster_ID    = 'JLO'
+    out_file_type = 'mat' # or npy 
+    logger        = setup_custom_logger('Main') # logger.info('Message') and logger.error('Message')
+    rainflow_func = get_range_and_count_qats
+    geo_matrix    = create_geo_matrix(geometry, sectors) # better matrix to pass to the main function
+    info_str      = "DEM" if DEM else "damage"
+    calc_func     = calculate_DEM_case_i if DEM else calculate_damage_case_i
+    TEN_MIN_TO_HR = 6.0 # to go from damage or DEM "per 10 min" to "per hr" 
 
-    logger.info(f'Initiating {info_str} calculation for the {cluster_ID} cluster, DLCs {DLC_IDs}')
-    elevations = [str(geo_matrix[i]['elevation']) + ' mLAT' for i in range(len(geo_matrix))]
-    logger.info(f'Settings: multiprocessed = {MULTIPROCESS}, {len(point_angles)} angles, {n_geometries} geometries: {elevations} \n')
+    elevations = [f'{geo_matrix[i]["elevation"]} mLAT' for i in range(len(geo_matrix))]
+    logger.info(f'Initiating {info_str} calculation for the {cluster_ID} cluster,\n\
+                DLCs {DLC_IDs}\n\
+                Multiprocessed = {MULTIPROCESS}, {len(sectors)} sectors, \n\
+                {n_geometries} elevations: {elevations} \n')
     
     for DLC_ID in DLC_IDs:
         # Collect relevant DLC data, find the probabilities of occurence of each case and the number of cases
         df, probs, n_cases, _ = extract_and_preprocess_data(DLC_file, DLC_ID, cluster_ID, sim_res_cluster_folder)
         output_file_name      = fr'{os.getcwd()}\output' + fr'\{info_str}_DB_{cluster_ID}_{DLC_ID}.{out_file_type}'
-        summary_table_DLC_i   = np.zeros((n_cases, n_geometries, len(point_angles))) # pre-allocate output matrix of the current DLC
-        range_storage_path    = fr'{os.getcwd()}\output\markov' + fr'\DB_{cluster_ID}_{DLC_ID}' + '_member{}'
+        summary_table_DLC_i   = np.zeros((n_cases, n_geometries, len(sectors))) # pre-allocate output matrix of the current DLC
+        cycle_storage_path    = fr'{os.getcwd()}\output\markov' + fr'\DB_{cluster_ID}_{DLC_ID}' + '_member{}'
         
         # Prepare the calculation function arguments which can be passed to any function, either multiprocessed or not
         arguments = [(df.results_files[i], 
                       df.descr_files[i], 
-                      point_angles, 
+                      sectors, 
                       geo_matrix, 
-                      curve,
                       rainflow_func,
-                      store_ranges,
-                      range_storage_path + f'_case{i}.npy'
+                      store_cycles,
+                      cycle_storage_path + f'_case{i}.npy'
                      ) for i in range(n_cases)]
         
         logger.info(f'Starting {info_str} calculation on {DLC_ID} with {n_cases} cases')
@@ -101,16 +98,15 @@ if __name__ == '__main__':
         else:
             for case_i in range(n_cases):
                 summary_table_DLC_i[[case_i], :, :] = calc_func(*arguments[case_i])
-                sys.exit()
-        
+                
         logger.info(f'Finished calculating {info_str} - initiating aggregating calculation and file storage')
         
         # Transform the summary table into a combined damage / DEM matrix of size (n_geometries, n_angles), 
-        # weighting each of the cases by their probabilities, and converting to hour-based damage according to "factor"
-        weighted_table_DLC_i = np.zeros((n_geometries, len(point_angles)))
+        # weighting each of the cases by their probabilities, and converting to hour-based damage according to "TEN_MIN_TO_HR"
+        weighted_table_DLC_i = np.zeros((n_geometries, len(sectors)))
         weights = np.array([probs])
-        for ang_idx in range(len(point_angles)):
-            weighted_table_DLC_i[:, [ang_idx]] = np.dot(weights, summary_table_DLC_i[:,:, ang_idx]).T * factor # (n_geometries, 1) -> multiplication of weights by dot product
+        for sector_idx in range(len(sectors)):
+            weighted_table_DLC_i[:, [sector_idx]] = np.dot(weights, summary_table_DLC_i[:,:, sector_idx]).T * TEN_MIN_TO_HR  # (n_geometries, 1) -> multiplication of weights by dot product
         
         # Now save the results for later usage     
         store_table(summary_table_DLC_i, weighted_table_DLC_i, weights, output_file_name, identifier = info_str) # Stores as .mat or .npy binary file
